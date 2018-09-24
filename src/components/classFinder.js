bd78528d58bd409c95b2dd4279df3dcbd74987de
Christopher Whiteman <cjszk@hotmail.com>
2018-09-24 12:43:03 -0700

    minor change

--- src/components/classFinder.js
+++ src/components/classFinder.js
@@ -101,6 +101,7 @@ class ClassFinder extends Component {
             </div>
         </div>
 
+
     render() {
         //Data for rendering purposes
         const teachers = sampleData.map((item) => {
@@ -131,13 +132,13 @@ class ClassFinder extends Component {
         function onlyUnique(value, index, self) { 
             return self.indexOf(value) === index && value !== undefined;
         }
-        const durations = this.state.data.map((item) => item.duration[0]).filter(onlyUnique).sort((a,b) => parseInt(a.replace(/[^0-9]/g, ''), 10) - parseInt(b.replace(/[^0-9]/g, ''), 10)).map((item) => item.replace('-', ' ').split(' ').map((item) => item[0].toUpperCase() + item.slice(1, item.length)).join(' '));
+        const durations = this.state.data.map((item) => item.duration[0]).filter(onlyUnique).sort((a,b) => parseInt(a.replace(/[^0-9]/g, ''), 10) - parseInt(b.replace(/[^0-9]/g, ''), 10));
         const durationList = this.buildList(durations, 'duration');
-        const levels = this.state.data.map((item) => item.level[0]).filter(onlyUnique).sort().map((item) => item.replace('-', ' ').replace('-', '/').split(' ').map((item) => item[0].toUpperCase() + item.slice(1, item.length)).join(' '));
+        const levels = this.state.data.map((item) => item.level[0]).filter(onlyUnique).sort();
         const levelList = this.buildList(levels, 'level');
-        const styles = this.state.data.map((item) => item.style[0]).filter(onlyUnique).sort().map((item) => item.replace('-', ' ').split(' ').map((item) => item[0].toUpperCase() + item.slice(1, item.length)).join(' '));
+        const styles = this.state.data.map((item) => item.style[0]).filter(onlyUnique).sort();
         const styleList = this.buildList(styles, 'style')
-        const bodyParts = this.state.data.map((item) => item.anatomical_focus[0]).filter(onlyUnique).sort().map((item) => item.replace('-', ' ').split(' ').map((item) => item[0].toUpperCase() + item.slice(1, item.length)).join(' '));
+        const bodyParts = this.state.data.map((item) => item.anatomical_focus[0]).filter(onlyUnique).sort();
         const bodyPartList = this.buildList(bodyParts, 'anatomical_focus')
 
         let filterList = [];
